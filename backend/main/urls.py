ec34c765c41a8d3738ff6dbead54cb536a40c7cf
lfyya <patrickjoshua2005@gmail.com>
2025-04-09 00:15:18 +0530

    Initial commit with Django backend and React frontend

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views  # import your views here
+from .api import hello_api
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('api/hello/', hello_api),
 ]
--- backend/my_profile/settings.py
+++ backend/my_profile/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'rest_framework',
+    'corsheaders',  
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'my_profile.urls'
@@ -121,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # React dev server
+]
